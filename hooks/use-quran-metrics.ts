ebb5486456663b060a7c393afe6437126f21fefb
Sarim <sarimyx@proton.me>
2025-12-23 22:11:57 -0700

    chore: mark progress

--- hooks/use-quran-metrics.ts
+++ hooks/use-quran-metrics.ts
@@ -1,16 +1,20 @@
+import { QueryType } from "wikisubmission-sdk";
+import { QueryResultMetadata } from "wikisubmission-sdk/lib/quran/v1/query-result";
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
 import { createJSONStorage } from "zustand/middleware"
 
 export type QuranMetrics = {
     lastChapter: number;
-    lastQuery: string;
+    lastQueryType?: QueryType;
+    lastQueryMetadata?: QueryResultMetadata;
     setMetrics: (metrics: QuranMetrics) => void;
 }
 
 export const useQuranMetrics = create(persist<QuranMetrics>((set) => ({
     lastChapter: 1,
-    lastQuery: "",
+    lastQueryType: undefined,
+    lastQueryMetadata: undefined,
     setMetrics: (metrics: QuranMetrics) => set(metrics),
 }), {
     name: "quran-metrics",
