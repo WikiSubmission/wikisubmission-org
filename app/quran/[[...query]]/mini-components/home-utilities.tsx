29697dbdaa7bb06914ba5f71e219f7fd4210d1a5
Sarim <sarimyx@proton.me>
2025-12-27 14:20:13 -0700

    chore: mark progress

--- app/quran/[[...query]]/mini-components/home-utilities.tsx
+++ app/quran/[[...query]]/mini-components/home-utilities.tsx
@@ -4,11 +4,23 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip
 import { ws } from "@/lib/wikisubmission-sdk";
 import { DownloadIcon, SparkleIcon } from "lucide-react";
 import Link from "next/link";
+import { Fonts } from "@/constants/fonts";
+import Image from "next/image";
 
 export default async function QuranUtilitiesRow() {
     const randomVerse = await ws.Quran.randomVerse();
     return (
-        <main className="p-2 flex items-center justify-end rounded-2xl space-x-4">
+        <main className="p-2 flex items-center justify-between rounded-2xl space-x-4">
+            <div>
+                <Link href="/quran">
+                    <div className={`flex items-center ${Fonts.geistMono.className} bg-muted/50 rounded-lg text-center`}>
+                        <Image src="/brand-assets/logo-transparent.png" alt="Quran" width={24} height={24} className="pb-0.5 pl-1" />
+                        <p className="p-2 text-xs">
+                            THE FINAL TESTAMENT
+                        </p>
+                    </div>
+                </Link>
+            </div>
             <div className="flex justify-end space-y-2 space-x-1">
                 <Tooltip>
                     <TooltipTrigger asChild>
