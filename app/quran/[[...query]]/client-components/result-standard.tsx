d686419893d040eb8b173553b9a864c007194100
Sarim <sarimyx@proton.me>
2025-12-27 19:07:05 -0700

    fix: add chapter 1/9 index adjustment

--- app/quran/[[...query]]/client-components/result-standard.tsx
+++ app/quran/[[...query]]/client-components/result-standard.tsx
@@ -8,11 +8,12 @@ import { StandardItemVerses } from "../mini-components/standard-item-verses";
 import Link from "next/link";
 
 export function StandardResult({ props }: { props: { query: string, data: QueryResultChapter | QueryResultVerse | QueryResultMultipleVerses } }) {
+    const indexAdjustment = ((props.data.data[0].chapter_number === 1 || props.data.data[0].chapter_number === 9) ? 0 : 1);
     return (
         <div className="space-y-4">
             <StandardItemTitle props={props} />
             <div>
-                {(props.data.data.length - 1) !== props.data.data[0].chapter_verses && (
+                {(props.data.data.length - indexAdjustment) !== props.data.data[0].chapter_verses && (
                     <Link href={`/quran/${props.data.data[0].chapter_number}?verse=${props.data.data[0].verse_number}`}>
                         <Button variant="secondary" size="sm">
                             <BookIcon />
