3373f70c5e9c897470ae58d6e18745e396691688
Sarim <sarimyx@proton.me>
2025-12-27 22:05:33 -0700

    chore: run update-types

--- types/supabase-types.ts
+++ types/supabase-types.ts
@@ -14,6 +14,198 @@ export type Database = {
   }
   public: {
     Tables: {
+      ws_media: {
+        Row: {
+          category: string
+          end_timestamp: string
+          index: number
+          start_timestamp: string
+          title: string
+          transcript: string
+          youtube_id: string
+          youtube_timestamp: string
+        }
+        Insert: {
+          category: string
+          end_timestamp: string
+          index?: number
+          start_timestamp: string
+          title: string
+          transcript: string
+          youtube_id: string
+          youtube_timestamp: string
+        }
+        Update: {
+          category?: string
+          end_timestamp?: string
+          index?: number
+          start_timestamp?: string
+          title?: string
+          transcript?: string
+          youtube_id?: string
+          youtube_timestamp?: string
+        }
+        Relationships: []
+      }
+      ws_music_albums: {
+        Row: {
+          artist: string
+          description: string | null
+          id: string
+          name: string
+          release_date: string
+        }
+        Insert: {
+          artist: string
+          description?: string | null
+          id?: string
+          name: string
+          release_date?: string
+        }
+        Update: {
+          artist?: string
+          description?: string | null
+          id?: string
+          name?: string
+          release_date?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "ws_music_albums_artist_fkey"
+            columns: ["artist"]
+            isOneToOne: false
+            referencedRelation: "ws_music_artists"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      ws_music_artists: {
+        Row: {
+          description: string | null
+          display_priority: number
+          id: string
+          image_url: string
+          name: string
+        }
+        Insert: {
+          description?: string | null
+          display_priority?: number
+          id?: string
+          image_url?: string
+          name: string
+        }
+        Update: {
+          description?: string | null
+          display_priority?: number
+          id?: string
+          image_url?: string
+          name?: string
+        }
+        Relationships: []
+      }
+      ws_music_categories: {
+        Row: {
+          description: string | null
+          display_priority: number
+          id: string
+          name: string
+        }
+        Insert: {
+          description?: string | null
+          display_priority?: number
+          id?: string
+          name: string
+        }
+        Update: {
+          description?: string | null
+          display_priority?: number
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
+      ws_music_tracks: {
+        Row: {
+          album: string | null
+          artist: string
+          category: string
+          featured: boolean
+          id: string
+          name: string
+          release_date: string
+          url: string
+        }
+        Insert: {
+          album?: string | null
+          artist: string
+          category: string
+          featured?: boolean
+          id?: string
+          name: string
+          release_date?: string
+          url: string
+        }
+        Update: {
+          album?: string | null
+          artist?: string
+          category?: string
+          featured?: boolean
+          id?: string
+          name?: string
+          release_date?: string
+          url?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "ws_music_tracks_album_fkey"
+            columns: ["album"]
+            isOneToOne: false
+            referencedRelation: "ws_music_albums"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "ws_music_tracks_artist_fkey"
+            columns: ["artist"]
+            isOneToOne: false
+            referencedRelation: "ws_music_artists"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "ws_music_tracks_category_fkey"
+            columns: ["category"]
+            isOneToOne: false
+            referencedRelation: "ws_music_categories"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      ws_newsletters: {
+        Row: {
+          content: string | null
+          html_tag: string
+          index: number
+          month: string
+          page: number
+          year: number
+        }
+        Insert: {
+          content?: string | null
+          html_tag: string
+          index?: number
+          month: string
+          page: number
+          year: number
+        }
+        Update: {
+          content?: string | null
+          html_tag?: string
+          index?: number
+          month?: string
+          page?: number
+          year?: number
+        }
+        Relationships: []
+      }
       ws_quran_appendices: {
         Row: {
           appendix_number: number
@@ -39,15 +231,18 @@ export type Database = {
           revelation_order: number
           title_arabic: string
           title_bahasa: string
+          title_bengali: string
           title_english: string
           title_french: string
           title_german: string
           title_persian: string
           title_russian: string
+          title_spanish: string
           title_swedish: string
           title_tamil: string
           title_transliterated: string
           title_turkish: string
+          title_urdu: string
         }
         Insert: {
           chapter_number: number
@@ -55,15 +250,18 @@ export type Database = {
           revelation_order: number
           title_arabic: string
           title_bahasa: string
+          title_bengali: string
           title_english: string
           title_french: string
           title_german: string
           title_persian: string
           title_russian: string
+          title_spanish?: string
           title_swedish: string
           title_tamil: string
           title_transliterated: string
           title_turkish: string
+          title_urdu?: string
         }
         Update: {
           chapter_number?: number
@@ -71,60 +269,72 @@ export type Database = {
           revelation_order?: number
           title_arabic?: string
           title_bahasa?: string
+          title_bengali?: string
           title_english?: string
           title_french?: string
           title_german?: string
           title_persian?: string
           title_russian?: string
+          title_spanish?: string
           title_swedish?: string
           title_tamil?: string
           title_transliterated?: string
           title_turkish?: string
+          title_urdu?: string
         }
         Relationships: []
       }
       ws_quran_footnotes: {
         Row: {
           bahasa: string
+          bengali: string
           chapter_number: number
           english: string
           french: string
           german: string
           persian: string
           russian: string
+          spanish: string
           swedish: string
           tamil: string
           turkish: string
+          urdu: string
           verse_id: string
           verse_index: number
           verse_number: number
         }
         Insert: {
           bahasa: string
+          bengali: string
           chapter_number: number
           english: string
           french: string
           german: string
           persian: string
           russian: string
+          spanish: string
           swedish: string
           tamil: string
           turkish: string
+          urdu: string
           verse_id: string
           verse_index?: number
           verse_number: number
         }
         Update: {
           bahasa?: string
+          bengali?: string
           chapter_number?: number
           english?: string
           french?: string
           german?: string
           persian?: string
           russian?: string
+          spanish?: string
           swedish?: string
           tamil?: string
           turkish?: string
+          urdu?: string
           verse_id?: string
           verse_index?: number
           verse_number?: number
@@ -191,45 +401,54 @@ export type Database = {
       ws_quran_subtitles: {
         Row: {
           bahasa: string
+          bengali: string
           chapter_number: number
           english: string
           french: string
           german: string
           persian: string
           russian: string
+          spanish: string
           swedish: string
           tamil: string
           turkish: string
+          urdu: string
           verse_id: string
           verse_index: number
           verse_number: number
         }
         Insert: {
           bahasa: string
+          bengali: string
           chapter_number: number
           english: string
           french: string
           german: string
           persian: string
           russian: string
+          spanish: string
           swedish: string
           tamil: string
           turkish: string
+          urdu: string
           verse_id: string
           verse_index?: number
           verse_number: number
         }
         Update: {
           bahasa?: string
+          bengali?: string
           chapter_number?: number
           english?: string
           french?: string
           german?: string
           persian?: string
           russian?: string
+          spanish?: string
           swedish?: string
           tamil?: string
           turkish?: string
+          urdu?: string
           verse_id?: string
           verse_index?: number
           verse_number?: number
@@ -263,16 +482,20 @@ export type Database = {
           arabic: string
           arabic_clean: string
           bahasa: string
+          bengali: string
           chapter_number: number
           english: string
           french: string
           german: string
           persian: string
+          persian_new: string
           russian: string
+          spanish: string
           swedish: string
           tamil: string
           transliterated: string
           turkish: string
+          urdu: string
           verse_id: string
           verse_index: number
           verse_number: number
@@ -280,17 +503,21 @@ export type Database = {
         Insert: {
           arabic: string
           arabic_clean: string
-          bahasa?: string
+          bahasa: string
+          bengali: string
           chapter_number: number
           english: string
-          french?: string
-          german?: string
-          persian?: string
-          russian?: string
-          swedish?: string
-          tamil?: string
+          french: string
+          german: string
+          persian: string
+          persian_new: string
+          russian: string
+          spanish?: string
+          swedish: string
+          tamil: string
           transliterated: string
-          turkish?: string
+          turkish: string
+          urdu: string
           verse_id: string
           verse_index?: number
           verse_number: number
@@ -299,16 +526,20 @@ export type Database = {
           arabic?: string
           arabic_clean?: string
           bahasa?: string
+          bengali?: string
           chapter_number?: number
           english?: string
           french?: string
           german?: string
           persian?: string
+          persian_new?: string
           russian?: string
+          spanish?: string
           swedish?: string
           tamil?: string
           transliterated?: string
           turkish?: string
+          urdu?: string
           verse_id?: string
           verse_index?: number
           verse_number?: number
@@ -417,116 +648,116 @@ type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
-    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof DatabaseWithoutInternals },
+  | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+  | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+  ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+    DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
   ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+    DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
-    ? R
-    : never
+  ? R
+  : never
   : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])
-    ? (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
-    : never
+    DefaultSchema["Views"])
+  ? (DefaultSchema["Tables"] &
+    DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+      Row: infer R
+    }
+  ? R
+  : never
+  : never
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof DatabaseWithoutInternals },
+  | keyof DefaultSchema["Tables"]
+  | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+  ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
+    Insert: infer I
+  }
+  ? I
+  : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
-    : never
+  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+    Insert: infer I
+  }
+  ? I
+  : never
+  : never
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof DatabaseWithoutInternals },
+  | keyof DefaultSchema["Tables"]
+  | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+  ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
+    Update: infer U
+  }
+  ? U
+  : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
-    : never
+  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+    Update: infer U
+  }
+  ? U
+  : never
+  : never
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
-    | keyof DefaultSchema["Enums"]
-    | { schema: keyof DatabaseWithoutInternals },
+  | keyof DefaultSchema["Enums"]
+  | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+  ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+  : never = never,
 > = DefaultSchemaEnumNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+  ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+  : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof DatabaseWithoutInternals },
+  | keyof DefaultSchema["CompositeTypes"]
+  | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
+  ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+  : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+  ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  : never
 
 export const Constants = {
   public: {
