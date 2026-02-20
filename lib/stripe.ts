cfdd89b8cf860a5456b1afc5b77962c5172d28d6
hichem <hichem.toumi.2001@gmail.com>
2026-02-20 22:41:35 +0100

    chore: updating stripe

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,11 +1,11 @@
-import Stripe from "stripe";
+import Stripe from 'stripe'
 
 export const stripe = () => {
-    const secretKey = process.env.STRIPE_SECRET_KEY
-    if (!secretKey) {
-        throw new Error("Stripe secret key not found");
-    }
-    return new Stripe(secretKey, {
-        apiVersion: "2025-12-15.clover",
-    });
+  const secretKey = process.env.STRIPE_SECRET_KEY
+  if (!secretKey) {
+    throw new Error('Stripe secret key not found')
+  }
+  return new Stripe(secretKey, {
+    apiVersion: '2026-01-28.clover',
+  })
 }
