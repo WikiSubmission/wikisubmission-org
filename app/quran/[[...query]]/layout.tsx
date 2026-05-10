b191da28342479ac79584e421271b14fd3cd1d1f
hichem <hichem.toumi.2001@gmail.com>
2026-05-10 11:41:59 +0100

    chore: removed quran progress

--- app/quran/[[...query]]/layout.tsx
+++ app/quran/[[...query]]/layout.tsx
@@ -65,7 +65,6 @@ export default async function QuranLayout({
                 </div>
                 <QuranSettings />
               </div>
-              <QuranReadingProgressBar />
             </header>
           )}
         </div>
